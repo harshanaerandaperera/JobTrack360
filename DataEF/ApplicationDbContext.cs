d2d539314f274b7dc7083ea8c2850a67ca1d96a4
harshanaerandaperera <harshanatxn@gmail.com>
2025-08-05 17:01:54 +1200

    Added initial EF core DbContext

---
+++ DataEF/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using JobTrack360.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTrack360.DataEF
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<JobApplication> JobApplications { get; set; }
+    }
+  
+}
