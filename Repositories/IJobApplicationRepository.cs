52c2b73e8b88c96da3e26bea249b68ea0bec01f4
harshanaerandaperera <harshanatxn@gmail.com>
2025-08-05 19:06:43 +1200

    + repository interface & implementation

---
+++ Repositories/IJobApplicationRepository.cs
@@ -0,0 +1,13 @@
+﻿using JobTrack360.Models;
+
+namespace JobTrack360.Repositories
+{
+    public interface IJobApplicationRepository
+    {
+        Task<IEnumerable<JobApplication>> GetAllAsync();
+        Task<JobApplication?> GetByIdAsync(int id);
+        Task<JobApplication> AddAsync(JobApplication app);
+        Task<JobApplication?> UpdateAsync(JobApplication app);
+        Task<JobApplication?> DeleteAsync(int id); 
+    }
+}
---
+++ Repositories/JobApplicationRepository.cs
@@ -0,0 +1,49 @@
+﻿using JobTrack360.DataEF;
+using JobTrack360.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTrack360.Repositories
+{
+    public class JobApplicationRepository : IJobApplicationRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public JobApplicationRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<JobApplication> AddAsync(JobApplication app)
+        {
+            _context.JobApplications.Add(app);
+            await _context.SaveChangesAsync();
+            return app;
+        }
+
+        public async Task<IEnumerable<JobApplication>> GetAllAsync() => await _context.JobApplications.ToListAsync();
+        
+        public async Task<JobApplication?> GetByIdAsync(int id)
+        {
+            return await _context.JobApplications.FindAsync(id);
+        }
+
+        public async Task<JobApplication?> UpdateAsync(JobApplication app)
+        {
+            var existing = await _context.JobApplications.FindAsync(app.Id);
+            if (existing == null) return null;
+
+            existing.Status = app.Status;
+            existing.Notes = app.Notes;
+            await _context.SaveChangesAsync();
+            return existing;
+        }
+        public async Task<JobApplication?> DeleteAsync(int id)
+        {
+            var existing = await _context.JobApplications.FindAsync(id);
+            if (existing == null) return null;
+
+            _context.JobApplications.Remove(existing);
+            await _context.SaveChangesAsync();
+            return existing;
+        }
+    }
+}
